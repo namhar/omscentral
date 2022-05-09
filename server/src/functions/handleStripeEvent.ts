144b103670330ce223e78972ff114a32e97421cb
Mehmet Bajin <10424048+mehmetbajin@users.noreply.github.com>
2022-05-09 08:42:03 -0400

    feat(freemium): handle more stripe events

--- server/src/functions/handleStripeEvent.ts
+++ server/src/functions/handleStripeEvent.ts
@@ -3,8 +3,16 @@ import assert from 'assert';
 import Stripe from 'stripe';
 
 import { logger, pp } from '../components';
+import { stripeConfig } from '../config';
 import { PricingTier } from '../enums';
+import { Maybe } from '../graphql';
 import { User } from '../models';
+
+const priceToTier: { [id: string]: PricingTier } = {
+  [stripeConfig.priceIds.basic]: PricingTier.Basic,
+  [stripeConfig.priceIds.standard]: PricingTier.Standard,
+  [stripeConfig.priceIds.premium]: PricingTier.Premium,
+};
 
 /**
  * Fulfills a customer's subscription order (by updating their user record).
@@ -13,9 +21,9 @@ const handleCheckoutSessionCompleted = async (event: Stripe.Event) => {
   try {
     // https://stripe.com/docs/api/checkout/sessions/object
     const session = event.data.object as Stripe.Checkout.Session;
-    assert(session != null, 'session must be nonnull');
-    assert(session.metadata != null, 'metadata must be nonnull');
-    assert(typeof session.customer === 'string', 'customer must be a string');
+    assert(session != null, 'session as nonnull');
+    assert(session.metadata != null, 'metadata as nonnull');
+    assert(typeof session.customer === 'string', 'customer as string');
 
     const pricingTier = session.metadata[pp.MetadataKey.PricingTier];
     assert(Object.values<string>(PricingTier).includes(pricingTier));
@@ -29,11 +37,42 @@ const handleCheckoutSessionCompleted = async (event: Stripe.Event) => {
   }
 };
 
+const handleCustomerSubscriptionUpdated = async (
+  event: Stripe.Event,
+  { deleted = false }: { deleted?: boolean } = {},
+) => {
+  try {
+    // https://stripe.com/docs/api/subscriptions/object
+    const subscription = event.data.object as Stripe.Subscription;
+    assert(subscription != null, 'subscription as nonnull');
+    assert(typeof subscription.customer === 'string', 'customer as string');
+    assert(subscription.items.data.length > 0, 'items nonempty');
+
+    let pricingTier: Maybe<PricingTier> = null;
+    if (!deleted) {
+      const [item] = subscription.items.data;
+      pricingTier = priceToTier[item.price.id];
+      assert(pricingTier != null, 'tier as nonnull');
+    }
+
+    await User.query()
+      .patch({ pricing_tier: pricingTier })
+      .where({ customer_id: subscription.customer });
+  } catch (error) {
+    logger.error('handleCheckoutSessionCompleted:', error);
+    Sentry.captureException(error);
+  }
+};
+
 export const handleStripeEvent = async (event: Stripe.Event) => {
   logger.debug('handleStripeEvent:', event);
   switch (event.type) {
     case 'checkout.session.completed':
       return handleCheckoutSessionCompleted(event);
+    case 'customer.subscription.updated':
+      return handleCustomerSubscriptionUpdated(event);
+    case 'customer.subscription.deleted':
+      return handleCustomerSubscriptionUpdated(event, { deleted: true });
     default: {
       Sentry.captureMessage(`handleStripeEvent: ${event.type}`);
       logger.warn(`handleStripeEvent: ${event.type}`);
